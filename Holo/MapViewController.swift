f0f2ace94960e4c856bb0b8b150fcce8e60b5e65
slipStas <41761920+slipStas@users.noreply.github.com>
2020-09-23 16:52:45 +0300

    add marker

--- Holo/MapViewController.swift
+++ Holo/MapViewController.swift
@@ -11,6 +11,7 @@ import GoogleMaps
 class MapViewController: UIViewController {
 
     let coordinateMoscovCenter = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
+    var marker: GMSMarker?
     
     @IBOutlet weak var mapView: GMSMapView!
     
@@ -20,12 +21,32 @@ class MapViewController: UIViewController {
         configureMap()
     }
     
+    @IBAction func toRedSquare(_ sender: Any) {
+        mapView.animate(toLocation: coordinateMoscovCenter)
+    }
+    @IBAction func addMarker(_ sender: Any) {
+        if marker == nil {
+            addMarker()
+        } else {
+            removeMarker()
+        }
+        
+    }
+    
     func configureMap() {
         let camera = GMSCameraPosition.camera(withTarget: coordinateMoscovCenter, zoom: 12)
         mapView.camera = camera
+        mapView.isTrafficEnabled = true
         mapView.mapType = .normal
     }
-    @IBAction func toRedSquare(_ sender: Any) {
-        mapView.animate(toLocation: coordinateMoscovCenter)
+    func addMarker() {
+        let marker = GMSMarker(position: coordinateMoscovCenter)
+        marker.map = mapView
+        marker.icon = GMSMarker.markerImage(with: .blue)
+        self.marker = marker
+    }
+    func removeMarker() {
+        self.marker?.map = nil
+        self.marker = nil
     }
 }
