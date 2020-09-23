6c8befad346e45e47ea764e3dbeae21b3590fa05
slipStas <41761920+slipStas@users.noreply.github.com>
2020-09-23 19:24:08 +0300

    1

--- Holo/MapViewController.swift
+++ Holo/MapViewController.swift
@@ -34,15 +34,28 @@ class MapViewController: UIViewController {
     }
     
     func configureMap() {
-        let camera = GMSCameraPosition.camera(withTarget: coordinateMoscovCenter, zoom: 12)
+        let camera = GMSCameraPosition.camera(withTarget: coordinateMoscovCenter, zoom: 16)
         mapView.camera = camera
         mapView.isTrafficEnabled = true
         mapView.mapType = .normal
     }
     func addMarker() {
         let marker = GMSMarker(position: coordinateMoscovCenter)
+        let rect = CGRect(x: 0, y: 0, width: 60, height: 60)
+        let markerView = UIView(frame: rect)
+        
+        markerView.backgroundColor = .cyan
+        markerView.layer.cornerRadius = markerView.frame.height / 2
+        markerView.layer.masksToBounds = false
+        markerView.clipsToBounds = false
+        markerView.layer.shadowRadius = 30
+        markerView.layer.shadowColor = UIColor.black.cgColor
+        markerView.layer.shadowOpacity = 1
+
         marker.map = mapView
-        marker.icon = GMSMarker.markerImage(with: .blue)
+        marker.iconView = markerView
+        marker.title = "Title"
+        marker.snippet = "This is the center of the world"
         self.marker = marker
     }
     func removeMarker() {
