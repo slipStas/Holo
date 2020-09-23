cf97ef33721cf62d0b1e2feaab3052ace64e3cf0
slipStas <41761920+slipStas@users.noreply.github.com>
2020-09-23 22:35:43 +0300

    add start/stop tracking button

--- Holo/MapViewController.swift
+++ Holo/MapViewController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
 class MapViewController: UIViewController {
 
     let coordinateMoscovCenter = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
     var marker: GMSMarker?
+    var manualMarker: GMSMarker?
+    var locationManager: CLLocationManager?
+    var isUpdateLocation = false
     
     @IBOutlet weak var mapView: GMSMapView!
     
@@ -19,18 +23,20 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
 
         configureMap()
+        configureLocationManager()
     }
     
-    @IBAction func toRedSquare(_ sender: Any) {
-        mapView.animate(toLocation: coordinateMoscovCenter)
-    }
-    @IBAction func addMarker(_ sender: Any) {
-        if marker == nil {
-            addMarker()
+    @IBAction func trackingLocation(_ sender: UIBarButtonItem) {
+        if !isUpdateLocation {
+            locationManager?.startUpdatingLocation()
+            sender.title = "Stop tracking"
+            self.isUpdateLocation = true
         } else {
-            removeMarker()
+            locationManager?.stopUpdatingLocation()
+            sender.title = "Start tracking"
+            self.isUpdateLocation = false
+            self.mapView.animate(toBearing: .zero)
         }
-        
     }
     
     func configureMap() {
@@ -38,6 +44,10 @@ class MapViewController: UIViewController {
         mapView.camera = camera
         mapView.isTrafficEnabled = true
         mapView.mapType = .normal
+        mapView.settings.myLocationButton = true
+        mapView.isMyLocationEnabled = true
+        
+        mapView.delegate = self
     }
     func addMarker() {
         let marker = GMSMarker(position: coordinateMoscovCenter)
@@ -56,10 +66,61 @@ class MapViewController: UIViewController {
         marker.iconView = markerView
         marker.title = "Title"
         marker.snippet = "This is the center of the world"
+        marker.groundAnchor = CGPoint(x: 0.5, y: 0.5)
         self.marker = marker
     }
     func removeMarker() {
         self.marker?.map = nil
         self.marker = nil
     }
+    
+    func configureLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.delegate = self
+    }
+}
+
+extension MapViewController: GMSMapViewDelegate {
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        print(coordinate)
+        
+        if let manualMarker = manualMarker {
+            manualMarker.position = coordinate
+        } else {
+            let marker = GMSMarker(position: coordinate)
+            marker.map = mapView
+            self.manualMarker = marker
+        }
+    }
+}
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.first as Any)
+        
+        guard let coordinates = locations.first else {return}
+        
+        mapView.animate(toLocation: coordinates.coordinate)
+        mapView.animate(toBearing: coordinates.course)
+        let marker = GMSMarker(position: coordinates.coordinate)
+        let rect = CGRect(x: 0, y: 0, width: 15, height: 15)
+        let markerView = UIView(frame: rect)
+        
+        markerView.backgroundColor = .blue
+        markerView.layer.cornerRadius = markerView.frame.height / 2
+        marker.groundAnchor = CGPoint(x: 0.5, y: 0.5)
+
+        marker.map = mapView
+        marker.iconView = markerView
+        self.marker = marker
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    
+    
 }
