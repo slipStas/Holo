ec230759b910b713ef537c94b9225dd5920ad44a
slipStas <41761920+slipStas@users.noreply.github.com>
2020-10-01 19:55:13 +0300

    add cheking is user authorised in LoginViewController

--- Holo/View Controllers/LoginViewController.swift
+++ Holo/View Controllers/LoginViewController.swift
@@ -13,6 +13,8 @@ class LoginViewController: UIViewController {
     var user: User?
     var usersArray: [User] = []
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var isUserAuthorised = false
+    let defaults = UserDefaults.standard
     
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -21,7 +23,16 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.passwordTextField.delegate = self
+        
+        isUserAuthorised = defaults.bool(forKey: "isLogin")
+        print(isUserAuthorised)
+        
         loadUsers()
+        
+        if isUserAuthorised {
+            goToNavigationController()
+        }
     }
     
     @IBAction func signIn(_ sender: Any) {
@@ -42,12 +53,8 @@ class LoginViewController: UIViewController {
         if self.usersArray.count > 0 {
             usersArray.forEach { (user) in
                 if user.login == self.loginTextField.text && user.password == self.passwordTextField.text {
-                    UserDefaults.standard.set(true, forKey: "isLogin")
-                    let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                    let navigationVC = storyBoard.instantiateViewController(withIdentifier: "navigationController") as! NavigationViewController
-                    navigationVC.modalPresentationStyle = .fullScreen
-                    self.present(navigationVC, animated: true, completion: nil)
-                    print("go to NavigationViewController")
+                    
+                    goToNavigationController()
                 } else {
                     print("Incorrect login or password")
                     self.systemInformationLabel.text = "Incorrect login or password"
@@ -103,5 +110,24 @@ class LoginViewController: UIViewController {
             print(error.localizedDescription)
         }
     }
+    
+    func goToNavigationController() {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationVC = storyBoard.instantiateViewController(withIdentifier: "navigationController") as! NavigationViewController
+        navigationVC.modalPresentationStyle = .fullScreen
+        
+        DispatchQueue.main.async {
+            self.present(navigationVC, animated: true, completion: nil)
+        }
+        defaults.set(true, forKey: "isLogin")
+        print("go to NavigationViewController")
+        print(isUserAuthorised)
+    }
+}
 
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
 }
